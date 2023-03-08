ed90200188266576f2f221c623e26d0f4d5c07e8
Dan Bowen <danbowen2677@gmail.com>
2023-03-07 19:20:17 -0500

    started getBrewery function

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,17 @@
 
+function getBrewery() {
+    fetch(
+        // 'fetch' data from the appropriate URL
+        'https://api.openbrewerydb.org/breweries?by_state=north_carolina&by_city=Durham&per_page=20'
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+
+        console.log(data)
+        });
+
+}
+
+getBrewery()
