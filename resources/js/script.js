e12d0b4856a398e9ae20914611932709b9d043a0
Dan Bowen <danbowen2677@gmail.com>
2023-03-09 12:27:11 -0500

    brewery data displays on the webpage

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,8 @@
 // Added in order to get weather data. 
 var openWeatherAPIKey = "357d278b4dc1d31d59f16e3afe69a945";
 
+var searchResultsEl = $("#search-results-container");
+
 // button to send state & city to getbrewery function
 // card container to have even listener attached (to help trigger weather API by clicking on the select button)
 var submitButton = document.getElementById("submit-button");
@@ -18,7 +20,8 @@ function getBrewery() {
         .then(function (data) {
           // array to be placed within local storage 
         console.log(data)
-        seachresultslist()
+        // seachresultslist()
+        displaydata(data)
         });
 
 }
@@ -27,10 +30,61 @@ getBrewery()
 
 
 
-function displaydata(data) {
-  // loop through array of all breweries
-  // for each brewery create a card (brewery name, city, address, phone #, state, zip code). Each card to have select button. (add latitude/longitude as data attributes to select button)
-  // append card to the card container 
+function displaydata(brewdata) {
+    console.log("The function to display the data on each brewery")
+    console.log(brewdata)
+
+      // loop through array of all breweries
+    for (var i=0; i<brewdata.length; i++){
+
+        // for each brewery create a card (brewery name, city, address, phone #, state, zip code). Each card to have select button. (add latitude/longitude as data attributes to select button)       
+        var brewContainerEl = $('<section>');
+        brewContainerEl.attr('id', 'brewery-id-'+i)
+        brewContainerEl.attr('class', 'col-sm bg-primary text-white m-1') 
+
+        
+        var breweryNameEl = $('<p>')
+        breweryNameEl.attr('id', 'brewery-name-id-'+i)
+        breweryNameEl.text('Name: ' + brewdata[i].name)
+
+        var breweryStreetEl = $('<p>')
+        breweryStreetEl.attr('id', 'brewery-street-id-'+i)
+        breweryStreetEl.text('Street: ' + brewdata[i].street)
+        
+        var breweryCityStateZipEl = $('<p>')
+        breweryCityStateZipEl.attr('id', 'brewery-city-state-zip-id-'+i)
+        breweryCityStateZipEl.text('Address: ' + brewdata[i].city + ', ' + brewdata[i].state + ' ' + brewdata[i].postal_code)
+
+        var breweryPhoneEl = $('<p>')
+        breweryPhoneEl.attr('id', 'brewery-phone-id-'+i)
+        breweryPhoneEl.text('Phone: ' + brewdata[i].phone)
+
+        var breweryTypeEl = $('<p>')
+        breweryTypeEl.attr('id', 'brewery-type-id-'+i)
+        breweryTypeEl.text('Brewery Type: ' + brewdata[i].brewery_type)
+
+        var checkWeatherBtnEl = $('<button>')
+        checkWeatherBtnEl.attr('type', 'button')
+        checkWeatherBtnEl.attr('id', 'check-weather-btn-id-'+i)
+        checkWeatherBtnEl.text('Select Brewery!')
+
+        brewContainerEl.append(breweryNameEl)
+        brewContainerEl.append(breweryCityStateZipEl)
+        brewContainerEl.append(breweryPhoneEl)
+        brewContainerEl.append(breweryTypeEl)
+        brewContainerEl.append(checkWeatherBtnEl)
+
+        // append card to the card container 
+        searchResultsEl.append(brewContainerEl)
+
+        //longitude and latitude data to pass to the weatheroutlook function when a city is selected.
+        var longitude = brewdata[i].longitude
+        var latitude = brewdata[i].latitude
+
+        weatheroutlook()
+
+    }
+
 }
 
 
@@ -40,15 +94,15 @@ function seachresultslist() {
   // use local storage to populate search results list
   // selector button to choose a specific brewery within the results list
   // pass long/lat to weather function
-    weatheroutlook()
+    // weatheroutlook()
 }
 
 // insert longitude and latitude variables into weatheroutlook function call
-function weatheroutlook() {
+function weatheroutlook(longitude, latitude) {
     console.log("Function to fetch the weather outlook")
   // get latitude/longitude from clicking select button 
-  var longitude = "-78.88891655" 
-  var latitude = "35.89445737"
+   var longitude = "-78.88891655" 
+   var latitude = "35.89445737"
 
   // fetch weather api 
   // use longitude/latitude to retrieve current weather for specific brewery
